5b6e2f5c53a4c7940c3aa23abf2cf78d7c2a9dc0
Christian Mihigo <mihigo@u.northwestern.edu>
2017-09-19 08:50:17 -0500

    day 2 start

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const volleyball = require('volleyball');
+const morgan = require('morgan');
+const chalk = require('chalk');
+const app = express(); // creates an instance of an express application
+
+
+
+app.use(volleyball);
+
+app.get('/*', function (req, res, next) {
+  // this is what will happen when GET /
+  // console.log(req.originalUrl);
+  res.send('Hello, welcome to our Twitter App!');
+  next();
+});
+
+
+app.listen(3000, function () {
+  console.log('Listen on 3000');
+});
