4925b6ab7c51d887ff8e1ce87790c9ce5d4f6a50
Christian Mihigo <mihigo@u.northwestern.edu>
2017-09-19 11:28:00 -0500

    half

--- app.js
+++ app.js
@@ -3,18 +3,41 @@ const volleyball = require('volleyball');
 const morgan = require('morgan');
 const chalk = require('chalk');
 const app = express(); // creates an instance of an express application
-
-
+const nunjucks = require('nunjucks');
+const routes = require('./routes');
 app.use(volleyball);
+app.use('/', routes);
+app.use(express.static('public'));
 
-app.get('/*', function (req, res, next) {
+
+var locals = {
+    title: 'Twitter',
+    people: [
+        { name: 'Gandalf' },
+        { name: 'Frodo' },
+        { name: 'Hermione' }
+      ]
+    };
+    nunjucks.render('index.html', locals, function (err, output) {
+        console.log(output);
+      });
+const people = [{ name: 'Full' }, { name: 'Stacker' }, { name: 'Son' }];
+
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
+
+
+app.get('/', function (req, res, next) {
   // this is what will happen when GET /
   // console.log(req.originalUrl);
-  res.send('Hello, welcome to our Twitter App!');
+  // res.send('Hello, welcome to our Twitter App!');
+  res.render('index', { title: 'Hall of Fame', people: people });
   next();
 });
 
 
 app.listen(3000, function () {
-  console.log('Listen on 3000');
+  console.log('Listening on 3000');
 });
